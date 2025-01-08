c970dd0d76fe544fcfbb85b33c58185a7356f58b
ThaliaSedai <anastasija.zidai@gmail.com>
2025-01-08 10:52:42 +0100

    Header and UserInput component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+
+export default function Header() {
+    return (
+        <div id="header">
+            <img src="investment-calculator-logo.png" alt="" />
+            <h1>Investment Calculator</h1>
+        </div>
+    )
+}
